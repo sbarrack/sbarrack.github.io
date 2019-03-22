7d343e85dc49c42bcfb1da785b9b41590029d1c3
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-03-21 20:48:55 -0700

    didnt work

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#body").load("home.html");
-    $(".link").click(function(){
-        $("#body").load($(this).attr("page")+".html");
-    });
-    $(".navbar a.right").click(function(){
-        $("#body").load($(this).attr("page")+".html");
-    });
-});
