b23b047a58eff19d0b6b602c9d707757e43c7d25
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-03-26 16:28:19 -0700

    try me

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+function changePage(page) {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("page").innerHTML = this.responseText;
+        }
+    };
+
+    xhttp.open("GET", page, true);
+    xhttp.send();
+}
+
 window.onload = function() {
     // sticky navbar
     const navbar = document.getElementById("nav");
     var sticky = navbar.offsetTop;
+
+    changePage('home.html');
 
     window.onscroll = () => {
         if (window.pageYOffset >= sticky) {
@@ -9,10 +24,10 @@ window.onload = function() {
         } else {
             navbar.classList.remove("sticky");
         }
-    }
+    };
 
     window.onresize = () => {
         sticky = navbar.offsetTop;
-    }
+    };
 
 }
