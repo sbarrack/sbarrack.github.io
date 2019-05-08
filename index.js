825fc840400c6aa99f2b6614833b42eec3143dd6
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-05-07 17:03:45 -0700

    fixed things

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 $(document).ready(function () {
+    $('.navbar-burger').click(toggleNav)
+    $('.navbar-item').click(toggleNav)
+})
 
-    $(".navbar-burger").click(function () {
-        toggleNav();
-
-        $(".icon").toggleClass("fa-times");
-        $(".icon").toggleClass("fa-bars");
-
-    });
-    $(".navbar-item").click(toggleNav());
-
-});
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault()
+    $('html, body').animate({scrollTop: $($.attr(this, 'href')).offset().top - 52}, 500)
+})
 
 function toggleNav() {
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
+    $('.navbar-burger').toggleClass('is-active')
+    $('.navbar-menu').toggleClass('is-active')
+    $('.icon').toggleClass('fa-times')
+    $('.icon').toggleClass('fa-bars')
 }
