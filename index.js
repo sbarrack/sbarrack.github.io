54e114b6f945f1b4c9f25f02145f2a6d30e17378
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-05-08 11:27:42 -0700

    bg
    stole this picture from google, not mine, just a placeholder

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     $('html, body').animate({scrollTop: $($.attr(this, 'href')).offset().top - 52}, 500)
 })
 
+$(window).resize(function () {
+    if ($(window).width() >= 1088) {
+        $('.navbar-burger').removeClass('is-active')
+        $('.navbar-menu').removeClass('is-active')
+        $('.icon').removeClass('fa-times').addClass('fa-bars')
+    }
+})
+
 function toggleNav() {
     $('.navbar-burger').toggleClass('is-active')
     $('.navbar-menu').toggleClass('is-active')
-    $('.icon').toggleClass('fa-times')
-    $('.icon').toggleClass('fa-bars')
+    $('.icon').toggleClass('fa-times').toggleClass('fa-bars')
 }
