84b461fe6723442334b9972011ae942e8ed36a65
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-05-10 10:21:22 -0700

    fixed more stuff

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-var count
-const end = 2
-
 $(document).ready(function () {
+    const end = 2
+    var count = 0
+
     $('.navbar-burger').click(toggleNav)
-    $('.navbar-item').click(toggleNav)
-    count = 0
+    $('.navbar-end .navbar-item').click(toggleNav)
     slideshow()
-})
 
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault()
-    $('html, body').animate({scrollTop: $($.attr(this, 'href')).offset().top - 52}, 500)
-})
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault()
+        $('html, body').animate({scrollTop: $($.attr(this, 'href')).offset().top - 52}, 500)
+    })
 
-$(window).resize(function () {
-    if ($(window).width() >= 1088) {
-        $('.navbar-burger').removeClass('is-active')
-        $('.navbar-menu').removeClass('is-active')
-        $('nav .icon').removeClass('fa-times').addClass('fa-bars')
+    $(window).resize(function () {
+        if ($(window).width() >= 1088) {
+            $('.navbar-burger').removeClass('is-active')
+            $('.navbar-menu').removeClass('is-active')
+            $('nav .icon').removeClass('fa-times').addClass('fa-bars')
+        }
+    })
+
+    function toggleNav() {
+        $('.navbar-burger').toggleClass('is-active')
+        $('.navbar-menu').toggleClass('is-active')
+        $('nav .icon').toggleClass('fa-times').toggleClass('fa-bars')
+    }
+
+    function slideshow() {
+        count = count > end ? 0 : count
+        $('body').css('background-image', 'url("images/bg/' + count++ + '.jpg")').show(0, function () {
+            setTimeout(slideshow, 5000)
+        })
     }
 })
-
-function toggleNav() {
-    $('.navbar-burger').toggleClass('is-active')
-    $('.navbar-menu').toggleClass('is-active')
-    $('nav .icon').toggleClass('fa-times').toggleClass('fa-bars')
-}
-
-function slideshow() {
-    count = count > end ? 0 : count
-    $('body').css('background-image', 'url("images/bg/' + count++ + '.jpg")').show(0, function () {
-        setTimeout(slideshow, 5000)
-    })
-}
