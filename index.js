34ee7497d7f4e15049cb6c85d54c24841c9f1545
Stephen <12619603+sbarrack@users.noreply.github.com>
2019-05-13 10:49:22 -0700

    simpler times
    make a bulma theme with a dark bg and lighter text

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ $(document).ready(function () {
 
     $('.navbar-burger').click(toggleNav)
     $('.navbar-end .navbar-item').click(toggleNav)
-    slideshow()
 
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault()
-        $('html, body').animate({scrollTop: $($.attr(this, 'href')).offset().top - 52}, 500)
+        $('html, body').animate({ scrollTop: $($.attr(this, 'href')).offset().top - 52 }, 500)
     })
 
     $(window).resize(function () {
@@ -23,12 +22,5 @@ $(document).ready(function () {
         $('.navbar-burger').toggleClass('is-active')
         $('.navbar-menu').toggleClass('is-active')
         $('nav .icon').toggleClass('fa-times').toggleClass('fa-bars')
-    }
-
-    function slideshow() {
-        count = count > end ? 0 : count
-        $('.hero').css('background-image', 'url("images/bg/' + count++ + '.jpg")').show(0, function () {
-            setTimeout(slideshow, 5000)
-        })
     }
 })
