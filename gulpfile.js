916a2d6681ab40cc82a4acba071d021c5b0a8cc1
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-06-11 21:33:40 -0700

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,40 @@
-'use strict';
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+gulp = require('gulp');
+sass = require('gulp-sass');
+sourcemaps = require('gulp-sourcemaps');
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function () {
-return gulp.src('./sass/style.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css'));
+gulp.task('sass', () => {
+    return gulp.src('./sass/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css'));
 });
 
-gulp.task('release', function () {
-return gulp.src('./sass/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+gulp.task('sass-release', () => {
+    return gulp.src('./sass/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('modules', () => {
+    sources = [
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/aos/dist/aos.js',
+        './node_modules/aos/dist/aos.css',
+        './node_modules/xterm/lib/xterm.js'
+    ]
+    return gulp.src( sources ).pipe(gulp.dest('./dist/'));
 });
  
-gulp.task('default', function () {
+gulp.task('default', () => {
     gulp.watch('./sass/style.scss', gulp.series('sass'));
+});
+
+gulp.task('release', () => {
+    return Promise.all([
+        gulp.series('modules'),
+        gulp.series('sass-release')
+    ]);
 });
