b4c0b20508ff52c350cd3e1dbdb9228bd69b5ac9
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-11-20 10:01:58 -0800

    metadata
    also updates and cleanup

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
 (function ($, AOS) {
-    $(document).ready(function () {
-        AOS.init(/* {
+    $(document).on('ready', function () {
+        AOS.init({
             offset: 200,
             duration: 600,
             easing: 'ease-in-sine',
             delay: 100,
-        } */);
-        $('.navbar-burger').click(toggleNav);
-        $('.navbar-end .navbar-item').click(toggleNav);
+        });
+        $('.navbar-burger').on('click', toggleNav);
+        $('.navbar-end .navbar-item').on('click', toggleNav);
 
         $(document).on('click', 'a[href^="#"]', function (e) {
             e.preventDefault();
-            $('html, body').animate({ scrollTop: $($.attr(this, 'href')).offset().top - $('.navbar').outerHeight(true) }, 500);
-        })
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top - $('.navbar').outerHeight(true)
+            }, 500);
+        });
 
-        $(window).resize(function () {
+        $(window).on('resize', function () {
             if ($(window).width() >= 1024) {
-                $('.navbar-burger').removeClass('is-active');
-                $('.navbar-menu').removeClass('is-active');
+                $('.navbar-burger, .navbar-menu').removeClass('is-active');
                 $('nav .icon').removeClass('fa-times').addClass('fa-bars');
             }
-        })
+        });
 
-        function toggleNav() { 
-            $('.navbar-burger').toggleClass('is-active');
-            $('.navbar').toggleClass('is-active');
-            $('.navbar-menu').toggleClass('is-active');
+        function toggleNav() {
+            $('.navbar-burger, .navbar, .navbar-menu').toggleClass('is-active');
             $('nav .icon').toggleClass('fa-times').toggleClass('fa-bars');
         }
 
