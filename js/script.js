143b72cb0cd59fa1a3bc169087f667ac869c2ad7
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-11-20 12:02:53 -0800

    fix bugs

--- js/script.js
+++ js/script.js
@@ -1,35 +1,60 @@
 (function ($, AOS) {
-    $(document).on('ready', function () {
-        AOS.init({
-            offset: 200,
-            duration: 600,
-            easing: 'ease-in-sine',
-            delay: 100,
-        });
-        $('.navbar-burger').on('click', toggleNav);
-        $('.navbar-end .navbar-item').on('click', toggleNav);
+  $(function () {
+    const $window = $(window);
+    const $scroll = $("html, body");
+    const $icon = $("nav .icon");
+    const $navbar = $(".navbar");
+    const $burger = $(".navbar-burger");
+    const $menu = $(".navbar-menu");
+    const $terminal = $("#terminal");
 
-        $(document).on('click', 'a[href^="#"]', function (e) {
-            e.preventDefault();
-            $('html, body').animate({
-                scrollTop: $($.attr(this, 'href')).offset().top - $('.navbar').outerHeight(true)
-            }, 500);
-        });
-
-        $(window).on('resize', function () {
-            if ($(window).width() >= 1024) {
-                $('.navbar-burger, .navbar-menu').removeClass('is-active');
-                $('nav .icon').removeClass('fa-times').addClass('fa-bars');
-            }
-        });
-
-        function toggleNav() {
-            $('.navbar-burger, .navbar, .navbar-menu').toggleClass('is-active');
-            $('nav .icon').toggleClass('fa-times').toggleClass('fa-bars');
-        }
-
-        // var xterm = new Terminal();
-        // xterm.open($('#terminal')[0]);
-        // xterm.write('Testing, testaroo...');
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: "ease-in-sine",
+      delay: 100,
     });
-})($, AOS);
+    if ($terminal.length) {
+      let xterm = new Terminal();
+      // xterm.open($terminal.get(0));
+      // xterm.write('Testing, testaroo...');
+    }
+
+    $burger.on("click", toggleNav);
+    $(".navbar-item").on("click", toggleNav);
+
+    $(document).on("click", 'a[href^="#"]', function (e) {
+      e.preventDefault();
+      $scroll.animate(
+        {
+          scrollTop:
+            $($.attr(this, "href")).offset().top - $navbar.outerHeight(),
+        },
+        500
+      );
+    });
+
+    $window.on("resize", function () {
+      if ($window.width() >= 1024) {
+        $burger.removeClass("is-active");
+        $menu.removeClass("is-active");
+        $icon.removeClass("fa-times").addClass("fa-bars");
+      }
+    });
+
+    if (document.readyState === "complete") {
+      $menu.addClass("enabled");
+    } else {
+      $window.on("load", function () {
+        $menu.addClass("enabled");
+      });
+    }
+
+    function toggleNav(e) {
+      $navbar.toggleClass("is-active");
+      $burger.toggleClass("is-active");
+      $menu.toggleClass("is-active");
+      $icon.toggleClass("fa-times").toggleClass("fa-bars");
+    }
+  });
+})(jQuery, AOS);
